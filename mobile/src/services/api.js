947a840aeb8734b72ff2e7b2b4ab8944d124d0e8
AlanCoosta <alancoosta2323@gmail.com>
2019-12-08 13:34:28 -0300

    fix

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
+/* baseURL
+  iphone: 'http://localhost:port_backend'
+  android:
+    - emulador studio: http://10.0.2.2:port'3333
+    - genymotion: http://10.0.3.2:port3333
+    -USB: IP da máquina na rede: http://192.168.1.107:3333
+*/
+
 const api = axios.create({
-  baseURL: 'http://10.0.3.2:3333',
+  baseURL: 'http://192.168.1.107:3333',
 });
 
 export default api;
