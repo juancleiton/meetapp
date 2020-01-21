4a76ec01e214628413d992744ad9750d0a4db1c7
AlanCoosta <alancoosta2323@gmail.com>
2020-01-21 14:25:21 -0300

    check

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-
 import { Switch } from 'react-router-dom';
-import Route from './Route';
-
-import SignIn from '../pages/SignIn';
-import SignUp from '../pages/SignUp';
-
-import Dashboard from '../pages/Dashboard';
-import Profile from '../pages/Profile';
 
 import CreateMeetup from '../pages/CreateMeetup';
-import EditMeetup from '../pages/EditMeetup';
+import Dashboard from '../pages/Dashboard';
 import DetailsMeetup from '../pages/DetailsMeetup';
+import EditMeetup from '../pages/EditMeetup';
+import Profile from '../pages/Profile';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import Route from './Route';
 
 export default function Routes() {
   return (
