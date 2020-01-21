385851d3e207f12f52571555b9cddaa6f22492d3
AlanCoosta <alancoosta2323@gmail.com>
2020-01-21 14:44:00 -0300

    check7

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+
 import { Form, Input } from '@rocketseat/unform';
-
-import { signUpRequest } from '~/store/modules/auth/actions';
-
 import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+import { signUpRequest } from '~/store/modules/auth/actions';
 
 const schema = Yup.object().shape({
   name: Yup.string().required('O nome é obrigatório'),
